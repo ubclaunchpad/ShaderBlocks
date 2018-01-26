0c5d5ff9da220ac4dc2deed72a937f6328887b36
Tristan <tristancalderbank@gmail.com>
2018-01-26 15:09:11 -0800

    picture version

--- js-prototype/index.js
+++ js-prototype/index.js
@@ -1,8 +1,25 @@
 const Interpreter = require('js-interpreter');
 
-const myInterpreter = new Interpreter('2 * 2');
-myInterpreter.run();
-
-console.log(myInterpreter.value);
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
 
+const initFunc = function(interpreter, scope) {
+
+    const draw = (x, y, width, height, style) => {
+        ctx.fillStyle = style;
+        ctx.fillRect(x, y, width, height, style);
+    }
+
+  interpreter.setProperty(scope, 'draw', interpreter.createNativeFunction(draw));
+};
+
+const btn = document.getElementById('button');
+
+btn.addEventListener('click', (e) => {
+    const editor = document.getElementById('editor');
+    const code = editor.value;
+
+    const myInterpreter = new Interpreter(code, initFunc);
+    myInterpreter.run();
+});
