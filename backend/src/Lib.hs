24a1ac7504c011018b334d2f6e8a8f0ff1623969
Steven Shih <stevenshih1997@gmail.com>
2018-01-27 14:20:20 -0800

    skeleton server and landing page endpoint

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
 
+import qualified Data.Text.IO as T
+import Control.Monad
+import Data.Char
+import System.IO
+import Network
+import Data.Time.LocalTime
+
+data RequestType = GET | POST deriving (Show)
+
 someFunc :: IO ()
-someFunc = putStrLn "Hello, World!"
+someFunc = T.putStrLn "Hello, World!"
---
+++ backend/src/Server.hs
@@ -0,0 +1,79 @@
+module Server
+    ( server
+		) where
+
+import Control.Monad
+import Data.Char
+import System.IO
+import System.Environment
+import Network
+import Data.Time.LocalTime
+
+data RequestType = GET | POST deriving (Show)
+data Request = Request { rtype :: RequestType, path :: String, options :: [(String,String)] }
+data Response = Response { version :: String, statuscode :: Int }
+
+instance Show Request where
+	show r = "Request { " ++ show((rtype r)) ++ " " ++ (path r)  ++ (foldl (\acc (k,v) -> acc ++ "\n  " ++ k ++ ": " ++ v) "" (options r)) ++ "\n}"
+
+instance Show Response where
+	show r = version(r) ++ " " ++ show(statuscode(r)) ++ " " ++ (case statuscode(r) of
+		100 -> "Continue"
+		200 -> "OK"
+		404 -> "Not Found") ++ "\r\n\r\n"
+
+fromString :: String -> RequestType
+fromString t = case t of
+	"GET" -> GET
+	"POST" -> POST
+
+respond :: Request -> Handle -> IO ()
+respond request handle = do
+	putStrLn $ show request
+	let response = Response {version = "HTTP/1.1", statuscode = 200}
+	hPutStr handle $ show(response)
+	time <- getZonedTime
+	hPutStr handle $ "Haskell says HELLO.\nThe time is currently " ++ show(time) ++ "\n\n\nHere is some info from your session:\n" ++ show(request)
+
+-- Testing a landing page
+respondLand :: Request -> Handle -> IO ()
+respondLand request handle = do
+	let response = Response {version = "HTTP/1.1", statuscode = 200}
+	hPutStr handle $ show(response)
+	hPutStr handle $ "Testing a Landing Page"
+
+--- This should really validate input or something. Separate validator? Or as-we-go?
+parseRequestHelper :: ([String], [(String,String)]) -> [(String,String)]
+parseRequestHelper ([], accum) = accum
+parseRequestHelper ((l:rest), accum) 
+	| (length (words l)) < 2 = accum
+	| otherwise = parseRequestHelper(rest, accum ++ [(reverse . tail . reverse . head . words $ l, unwords . tail . words $ l)] )
+
+parseRequest :: [String] -> Request
+parseRequest lns = case (words (head lns)) of
+	[t,p,_] -> Request {rtype=(fromString t), path=p, options=parseRequestHelper((tail lns),[])}
+
+handleAccept :: Handle -> String -> IO ()
+handleAccept handle hostname = do 
+	putStrLn $ "Handling request from " ++ hostname
+	request <- fmap (parseRequest . lines) (hGetContents handle)
+	case path request of
+		"/landing" -> do
+			respondLand request handle
+			return ()
+		_ 	 -> do
+			putStrLn $ "path: " ++ (path request)
+			respond request handle
+			return ()
+
+server = withSocketsDo $ do
+	args <- getArgs
+	case args of
+		["sanity"] -> putStrLn "Sanity check passed"
+		[] -> do 
+			sock <- listenOn (PortNumber 9000)
+			putStrLn "Listening on port 9000"
+			forever $ do
+				(handle, hostname, port) <- accept sock
+				handleAccept handle hostname
+				hClose handle
